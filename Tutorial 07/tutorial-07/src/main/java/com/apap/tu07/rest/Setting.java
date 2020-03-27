154885490f82664d899597d686638849d5522119
Agung Tresna <agungtresna@yahoo.com>
2020-03-27 23:15:43 +0700

    tutorial-07

---
+++ Tutorial 07/tutorial-07/src/main/java/com/apap/tu07/rest/Setting.java
@@ -0,0 +1,8 @@
+package com.apap.tu07.rest;
+
+public class Setting {
+	final public static String pilotUrl = "https://50d70093-6493-4b29-985e-f6c8b933f072.mock.pstmn.io";
+	final public static String airportUrl = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete";
+	final public static String airportApikey = "oDl3hFiql6NAzflnStiL8XJj7jLngeLj/n";
+
+}
